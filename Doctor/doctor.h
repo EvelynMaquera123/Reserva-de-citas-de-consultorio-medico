ad933d5abcf74a5ad04ec5e3c0ada8ee9e1c74bb
MaximusJoshep <66806658+MaximusJoshep@users.noreply.github.com>
2020-06-12 12:30:07 -0500

    Update doctor.h

--- Doctor/doctor.h
+++ Doctor/doctor.h
@@ -8,9 +8,11 @@ class Doctor: public Persona
         string codigo;
         Especialidad especialidades[];
         Horario horarios[];
+    public:
+         Doctor::Doctor();//Constructor
+        ~Doctor::Doctor();
+        void crearHorario();//Con este metodo el doctor podra crear un nuevo horario
+        void consultarCitas();//Con este metodo el doctor consultara todas sus citas que tiene que atender
+        void consultarHorario();//Con este metodo el docto consultara su horario de consultas;
 }
-Doctor::Doctor();//Constructor
-~Doctor::Doctor();
-void crearHorario();//Con este metodo el doctor podra crear un nuevo horario
-void consultarCitas();//Con este metodo el doctor consultara todas sus citas que tiene que atender
-void consultarHorario();//Con este metodo el docto consultara su horario de consultas;
+
