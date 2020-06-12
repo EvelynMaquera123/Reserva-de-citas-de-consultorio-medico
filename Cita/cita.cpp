d7a559fc33325f30c036549e92b1062320e64df8
AngelCaceresPari01 <66806123+AngelCaceresPari01@users.noreply.github.com>
2020-06-12 14:59:29 -0500

    Create cita.cpp

---
+++ Cita/cita.cpp
@@ -0,0 +1,18 @@
+#include "cita.h"
+
+Cita::Cita()//Constructor
+{
+	
+}
+~Cita::Cita();
+{
+	
+}
+void revisarHorario();//Con este metodo se podra cambiar el horario de la cita
+{
+	
+}
+void reservarCita()();//Con este metodo se cambiara el estado de la cita
+{
+	
+}
