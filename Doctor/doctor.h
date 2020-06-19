02d1f31d3b75c388bb0e3f8f26085af8ac16dc8b
MaximusJoshep <66806658+MaximusJoshep@users.noreply.github.com>
2020-06-18 23:40:14 -0500

    Update doctor.h

--- Doctor/doctor.h
+++ Doctor/doctor.h
@@ -1,18 +1,20 @@
-
+#include "Especialidad.cpp"
+#include "horario.cpp"
+#include "Persona.cpp"
+#include <list>
 using namespace std;
 class Doctor: public Persona
 {
-    Doctor();
-    ~Doctor();
     private:
         string codigo;
-        Especialidad especialidades[];
-        Horario horarios[];
+        Especialidad especialidad;
+        std::list<Horario> horarios;//Creamos una lista de horarios ya que no sabemos con exactitud cuantos tendremos
     public:
-         Doctor::Doctor();//Constructor
-        ~Doctor::Doctor();
-        void crearHorario();//Con este metodo el doctor podra crear un nuevo horario
-        void consultarCitas();//Con este metodo el doctor consultara todas sus citas que tiene que atender
-        void consultarHorario();//Con este metodo el docto consultara su horario de consultas;
-}
+     //Constructor le tenemos que pasar todos los parametros de personas , para poder usar el constructor de persona
+        Doctor(char* nombr,char* apellidoP, char* apellidoM, Fecha fec,double peso,double estatura, char* sex, char* telef,char* direccion,string cod , Especialidad es,list<Horario> ho);
+        void mostrarDatos();//Con este metodo mostramos los datos del doctor
+        void consultarHorarios();//Mostraremos los horarios correspondientes al doctor
+        void crearHorario();//Crearemos un nuevo horario para este doctor
+};
+
 
