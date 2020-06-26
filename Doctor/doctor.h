2400c2d08e9789aed4e161d3ddf02ea03cbeab94
MaximusJoshep <66806658+MaximusJoshep@users.noreply.github.com>
2020-06-25 20:34:24 -0500

    ficheros_doctor_horario

--- Doctor/doctor.h
+++ Doctor/doctor.h
@@ -1,18 +1,27 @@
-#include "Especialidad.cpp"
-#include "horario.cpp"
-#include "Persona.cpp"
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../Especialidad/especialidad.cpp"
+#include "../Horario/horario.cpp"
+#include "../Persona/Persona.cpp"
 #include <list>
 using namespace std;
 class Doctor: public Persona
 {
     private:
-        string codigo;
+        std::string codigo;
         Especialidad especialidad;
         std::list<Horario> horarios;//Creamos una lista de horarios ya que no sabemos con exactitud cuantos tendremos
     public:
+       Doctor();
      //Constructor le tenemos que pasar todos los parametros de personas , para poder usar el constructor de persona
         Doctor(char* nombr,char* apellidoP, char* apellidoM, Fecha fec,double peso,double estatura, char* sex, char* telef,char* direccion,string cod , Especialidad es,list<Horario> ho);
         void mostrarDatos();//Con este metodo mostramos los datos del doctor
         void consultarHorarios();//Mostraremos los horarios correspondientes al doctor
         void crearHorario();//Crearemos un nuevo horario para este doctor
+        void crearDoctor();//Con este metodo insertaremos un nuevo doctor y lo añadiremos al fichero
 };
--- Horario/horario.h
+++ Horario/horario.h
@@ -1,4 +1,4 @@
-#include "Fecha.cpp"
+#include "../Fecha/Fecha.cpp"
 using namespace std;
 class Horario
 {
@@ -6,9 +6,11 @@ private:
     string codigo_doctor; //Usamos esta clave para identificar al doctor ya que es unica
     Fecha fecha;
 public:
+     Horario();//Constructor basico
      Horario(int anio,int mes,int dia,string cod);
     void modificar();//Con este metodo modifica la fecha del horario
     void mostrarDatos();//Mostramos todos los datos del horario
+    void crearHorario();//Con este metodo creamos un nuevo horario
 };
 
 
