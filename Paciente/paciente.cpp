fce2d5227c5906a37f05a0093de8631167f5b760
gaeladrian <jhonccamerccoaperez1211@gmail.com>
2020-06-26 12:37:30 -0500

    actualizacion modulo paciente

--- Paciente/paciente.cpp
+++ Paciente/paciente.cpp
@@ -1,14 +1,36 @@
 #include<iostream>
-#include<cstdlib>
-#include<string.h>
 #include "paciente.h"
+using namespace std;
+//struct Paciente pacientes;
+//Le pasamos los valores de nuestros parametros  exculisivos de Doctor
+Paciente::Paciente(char* nombr,char* apellidoP, char* apellidoM, Fecha fec,double peso,double estatura, char* sex, char* telef,char* direccion,string cod,list<Persona> pe)
+{
+  Persona( nombr,apellidoP, apellidoM, fec, peso,estatura, sex, telef,direccion);
+    //Le pasamos los valores de nuestros parametros  exculisivos de Paciente
+  codigo=cod;
 
-struct Paciente pacientes;
+  for(std::list<Persona>::iterator it = pe.begin();it != pe.end();++it)//Hacemos un for para iterar en cada uno de los objetos de la lista de horarios que le creamos
+        pacientes.push_back(*it);//Enrolamos cada Persona en la lista a la lista del Administrador
+ }
+ Paciente::Paciente()
+ {
+ }	
+void Paciente::registrarPaciente(){
+    
+}
+void Paciente::mostrarDatosPaciente() 
+{
+	//Datos personales
 
-void solicitarCita(paciente[]);
-void cancelarCita(paciente[]);
-void actualizarPacientes(paciente[]);
-void operacion(paciente[]);
+	cout << "Nombre: " << nombre <<"Apellido Paterno: "<< apellidoPaterno<<"Apellido Materno: "<<apellidoMaterno<<endl;
+	cout << "Fecha Nacimiento: " <<fechaNacimiento.a�o<<"/"<<fechaNacimiento.mes<<"/"<<fechaNacimiento.dia<<endl;
+	cout << "Peso: " << peso <<endl;
+	cout << "Edad: " << edad << endl;
+	cout << "Estatura: " << estatura << endl;
+	cout << "Sexo: "<<sexo << endl;
+	cout << "Peso: " << telefono<< endl;
+	cout << "Peso: " << direccion << endl;
+}
 
 void menu(void){  //CREAMOS UN MENU PARA LA GESTION DE LOS PACIENTES
 
@@ -22,145 +44,12 @@ void menu(void){  //CREAMOS UN MENU PARA LA GESTION DE LOS PACIENTES
 
     cout<<"\n Ingrese opcion : ";
 }
-
 //desarrollar los metodos
-
-void paciente::solicitarCita(paciente pacientes[5]){   //METODO EN EL CUAL EL USUARIO SOLICITA UNA CITA
-	
-	
-	cout<<"\n\n\t\t[  REGISTRO  ]\n";
-    cout<<"\t\t------------";
-    cout<<"\n\tDATOS DEL PACIENTE ";
-    for(int i=0;i<5;i++){
-    	
-    	cout<<"Codigo:" <<endl;
-        cin.getline(pacientes[i].codigo,20);
-    	cout<<"Nombre de la persona:" <<endl;
-        cin.getline(pacientes[i].nombre,20);
-        cout<<"Apellido Paterno" <<endl;
-        cin.getline(pacientes[i].apellidoPaterno,20);
-        cout<<"Apellido Paterno:" <<endl;
-        cin.getline(pacientes[i].apellidoMaterno,20);
-        cout<<"Sexo:" <<endl;
-        cin.getline(pacientes[i].sexo,20);
-        cout<<"Telefono:" <<endl;
-        cin.getline(pacientes[i].telefono,20);
-        cout<<"direccion:" <<endl;
-        cin.getline(pacientes[i].direccion,20);
-	
+void Paciente::solicitarCita(){   //METODO EN EL CUAL EL USUARIO SOLICITA UNA CITA
 	}
-	
-	
-void paciente::cancelarCita(paciente pacientes[5]){  //SI EL USUARIO DESEA CANCELAR SU CITA
-	 int cod;
-	
-	 cout<<"\n\n\n\tCANCELAR UNA CITA";
-    cout<<"\n\n\tINGRESE CODIGO:"; cin>>cod;
-   for(int i=0;i<5;++i){
- 
-    while(pacientes[i]!=NULL){
-
-            if(pacientes[i].codigo==cod){
-
-                if(pacientes[i].codigo==lista)
-                   //FALTA IMPLEMENTAR
-
-                else
-                    //FALTA IMPLEMENTAR
-
-                delete(q);
-
-                cout<<"\n\nCITA ELIMINADO...!!!!!\n";
-
-                return;
-
-            }else {
- //FALTA IMPLEMENTAR
-
-        }
-
-    }
-    if(paciente[i].codigo==NULL)
-        cout<<"\n\tCODIGO INCORRECTO...!!\n";
-
+void Paciente::cancelarCita(){ //metodo para cancelar cita del paciente
 }
-
-}
-
-void menu_actualizar(void){   //MEMU PARA ACTUALIZAR EL USUARIO TIENE PARA ESCOJER LOS DIFERENTES OPCIONES
-
-    cout<<"\n\t\t[    ESPECIFIQUE CAMPO A ACTUALIZAR    ]\n";
-    cout<<"\t\t----------------------------\n\n";
-    cout<<" 1. NOMBRES                       "<<endl;
-    cout<<" 2. APELLIDOS                     "<<endl;
-    cout<<" 3. DIRECCION                     "<<endl;
-    cout<<" 4. TELEFONO                      "<<endl;
-    cout<<" 5. SALIR                         "<<endl;
-
-    cout<<"\n Ingrese opcion : ";
-}
-void actualizar_paciente(paciente pacientes[5]{
-
-    int cod, x;  
-    
-
-    cout<<"\n\n\n\tACTUALIZAR REGISTRO DE PACIENTE";
-    cout<<"\n\n\tINGRESE CODIGO:"; cin>>cod;
-
-    while(pacientes!=NULL){
-
-            if(pacientes->codigo==cod){
-                system("cls");
-                cout<<"\n\tDATOS DEL PACIENTE  ";
-                cout<<"\n\t--------------------";
-                cout<<"\n\n\tCODIGO   : "<<pacientes->codigo<<endl;
-                cout<<"\n\tNOMBRES  : "<<pacientes->nombres<<endl;
-                cout<<"\tAPELLIDOS: "<<pacientes->apellidos<<endl;
-                cout<<"\tDIRECCION: "<<pacientes->direccion<<endl;
-                cout<<"\tTELEFONO : "<<pacientes->telefono<<endl;
-
-                menu_actualizar();
-                cin>>x;
-
-                switch(x){
-
-                    case 1: cout<<"\n\n\tINGRESE NOMBRES:";
-                            cin.ignore(); cin.getline(q->nombres,maxchar);
-                            break;
-
-                    case 2: cout<<"\n\n\tINGRESE APELLIDOS:";
-                            cin.ignore(); cin.getline(q->apellidos,maxchar);
-                            break;
-
-                    case 3: cout<<"\n\n\tINGRESE DIRECCION:";
-                            cin.ignore(); cin.getline(q->direccion,maxchar);
-                            break;
-
-                    case 4: cout<<"\n\n\tINGRESE TELEFONO:";
-                            cin>>q->telefono;
-                            break;
-
-                    default: cout<<"\nINGRESE UNA OPCION VALIDA...\n"; break;
-
-                }
-                cout<<"\n\n\tREGISTRO ACTUALIZADO...!!!!!\n";
-
-                return;
-
-            }else {
-
-
-                paciente=paciente->sgte;
-
-        }
-
-    }
-    if(q==NULL)
-        cout<<"\n\tCODIGO INCORRECTO...!!\n";
-}
-void operacion(Paciente pacientes[5]){
-	
- //falta implementar
+void Paciente::operacion(){ //metodo para 
 }
 
 
--- Paciente/paciente.h
+++ Paciente/paciente.h
@@ -1,21 +1,28 @@
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../Persona/Persona.cpp"
+#include <list>
+using namespace std;
 
-
-class Paciente{
+class Paciente: public Persona{
 	private:
-	string codigo[20];
-	string nombre[20];
-	string apellidoPaterno[20];
-	string apellidoMaterno[20];
-	char sexo;
-	char telefono[10];
-	string direccion[20];	
-	
+	std::string codigo;
+	std::list<Persona> pacientes;//Creamos una lista de pacientes ya que no sabemos con exactitud cuantos tendremos
 	public:
-	void registrarPaciente();
-	void solicitarCita();
-    void cancelarCita();
-	void operacion();
-	void actualizarPacientes();
+	Paciente(); //constructor donde tenemos que pasar los atributos
+	Paciente(char* nombr,char* apellidoP, char* apellidoM, Fecha fec,double peso,double estatura, char* sex, char* telef,char* direccion,string codidgo,list<Persona> pe);
+
+	void registrarPaciente(); //Registrar datos del paciente
+	void mostrarDatosPaciente(); //mostrar los datos del paciente
+	void solicitarCita();  //solicitar datos del paciente
+    void cancelarCita();   //cancelar cita del paciente
+	void operacion();    //Operaciones 
+	void actualizarPacientes(); //actualizar datos del paciente
 	
 };
 
