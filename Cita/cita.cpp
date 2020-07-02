edfbd2a0586c2225ab17ee4950489d2a69400496
AngelCaceresPari01 <66806123+AngelCaceresPari01@users.noreply.github.com>
2020-07-02 16:52:17 -0500

    arreglo
    a

--- Cita/cita.cpp
+++ Cita/cita.cpp
@@ -1,35 +1,36 @@
 #include "cita.h"
 #include <iostream>
-<<<<<<< Updated upstream
-/* ddvv este es un comentarioo*/
-=======
-/* ddvv*/
->>>>>>> Stashed changes
+
 Cita::Cita(){
-	
+
 }
-Cita::Cita(Doctor doctoraux, Paciente pacienteaux, Fecha fechaaux ,Especialidad especialidadaux,bool estadoaux)//Constructor
+//Constructor de la clase cita
+Cita::Cita(Doctor doctoraux, Paciente pacienteaux, Fecha fechaaux ,Especialidad especialidadaux,bool estadoaux)
 {
-	
 	doctor= doctoraux;
 	paciente= pacienteaux;
 	fecha= fechaaux;
 	especialidad= especialidadaux;
 	estado= estadoaux;
+	
 }
-
-void mostrarCita(){
-	cout << "Datos del Doctor: " << doctor.nombre << doctor.apellidoPaterno << doctor.apellidoMaterno << endl;
-	cout << "Datos del Paciente: " << paciente.nombre << paciente.apellidoPaterno << paciente.apellidoMaterno << endl;
-	cout << "Especialidad: " << especialidad.nombre << endl;
-	//cout<<"Fecha: "<<fecha.mostrarFecha<<endl;
+//Se muestran los datos de la cita
+void Cita:: mostrarCita(){
+	cout << "Datos del Doctor: " << endl;
+		doctor.mostrarDatos(); 
+	cout << "Datos del Paciente: " <<endl;
+		paciente.mostrarDatosPaciente();
+	cout << "Especialidad: " << endl;
+		//especialidad.mostrarEspecialidad();
+	cout<<"Fecha: "<<endl;
+		//fecha.mostrarFecha();
 	cout << "Estado: " << estado << endl;
 }
-void revisarHorario(Fecha aux) //Con este metodo se podra cambiar el horario de la cita
+void Cita:: revisarHorario(Fecha aux) //Con este metodo se podra cambiar el horario de la cita
 {
 	fecha = aux;
 }
-void reservarCita(bool aux) //Con este metodo se cambiara el estado de la cita
+void Cita ::reservarCita(bool aux) //Con este metodo se cambiara el estado de la cita
 {
 	estado = aux;
 }
